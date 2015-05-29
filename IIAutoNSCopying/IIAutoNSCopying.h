e09f77d08e31228b181957bc64c1cf868012f0bd
Inferis <spam@inferis.org>
2015-05-29 11:10:56 +0200

    Added IIAutoNSCopying.

---
+++ IIAutoNSCopying/IIAutoNSCopying.h
@@ -0,0 +1,13 @@
+//
+//  IIAutoNSCopying.h
+//  AutoNSCopyingDemo
+//
+//  Created by Tom Adriaenssen on 29/05/15.
+//  Copyright (c) 2015 Tom Adriaenssen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IIAutoNSCopying : NSObject
+
+@end
---
+++ IIAutoNSCopying/IIAutoNSCopying.m
@@ -0,0 +1,13 @@
+//
+//  IIAutoNSCopying.m
+//  AutoNSCopyingDemo
+//
+//  Created by Tom Adriaenssen on 29/05/15.
+//  Copyright (c) 2015 Tom Adriaenssen. All rights reserved.
+//
+
+#import "IIAutoNSCopying.h"
+
+@implementation IIAutoNSCopying
+
+@end
